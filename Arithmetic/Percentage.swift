6ca0bde6e60a55b621e1b0e1d99631b5a1e096e4
Alonso del Arte <alonso.delarte@gmail.com>
2024-01-20 15:58:54 -0500

    Passing description test

--- Arithmetic/Percentage.swift
+++ Arithmetic/Percentage.swift
@@ -9,14 +9,19 @@ import Foundation
 
 struct Percentage : CustomStringConvertible {
     
-    let description: String = "SORRY"
+    let number: Int
     
-    init(_ num: Int) {
-        //
+    var description: String {
+        "\(self.number)%"
     }
     
+    init(_ num: Int) {
+        self.number = num
+    }
+    
+    // TODO: Write tests for this
     init(_ num: Decimal) {
-        //
+        self.number = -1
     }
     
 }
