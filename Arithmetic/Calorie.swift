6946e624db5055f6ca13cb4171f739747f276604
Alonso del Arte <alonso.delarte@gmail.com>
2024-01-26 18:20:06 -0500

    Passing plus test

--- Arithmetic/Calorie.swift
+++ Arithmetic/Calorie.swift
@@ -11,12 +11,10 @@ struct Calorie : AdditiveArithmetic, CustomStringConvertible, Equatable {
     
     let number: Int
     
-    // TODO: Write tests for this
     static var zero: Calorie = Calorie(0)
     
-    // TODO: Write tests for this
     static func + (lhs: Calorie, rhs: Calorie) -> Calorie {
-        rhs
+        Calorie(lhs.number + rhs.number)
     }
     
     // TODO: Write tests for this
